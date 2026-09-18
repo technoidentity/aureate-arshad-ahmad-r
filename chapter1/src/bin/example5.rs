2022190766c8c346ece6134c27824b21722de724
Arshadahamed45 <arshadahmadr@Technoidentity.com>
2026-09-19 02:31:30 +0530

    Chapter 1 Example 5

---
+++ chapter1/src/bin/example5.rs
@@ -0,0 +1,10 @@
+fn main(){
+   let a=10;
+   let b=5;
+   
+   println!("Addition: {}", a+b);
+   println!("Subtraction: {}", a-b);
+   println!("Multiplication: {}", a*b);
+   println!("Division: {}", a/b);
+
+}
