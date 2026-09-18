17cf7680ef6bcb549c528f2d70dd28b328d4748a
Arshadahamed45 <arshadahmadr@Technoidentity.com>
2026-09-19 00:15:40 +0530

    Rust basics

---
+++ chapter1/target/debug/chapter1.d
@@ -0,0 +1,1 @@
+/home/technoidentity/aureate-arshad-ahmad-r/chapter1/target/debug/chapter1: /home/technoidentity/aureate-arshad-ahmad-r/chapter1/src/main.rs
---
+++ chapter1/target/debug/deps/chapter1-09e99b33c2b0fc9a.d
@@ -0,0 +1,5 @@
+/home/technoidentity/aureate-arshad-ahmad-r/chapter1/target/debug/deps/chapter1-09e99b33c2b0fc9a.d: src/main.rs
+
+/home/technoidentity/aureate-arshad-ahmad-r/chapter1/target/debug/deps/chapter1-09e99b33c2b0fc9a: src/main.rs
+
+src/main.rs:
