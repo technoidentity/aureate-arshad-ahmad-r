f450377f1d32259a3c27d103bb8b965bc6c48f7b
Arshadahamed45 <arshadahmadr@Technoidentity.com>
2026-09-19 02:23:15 +0530

    Chapter 1 Example 4

---
+++ chapter1/src/bin/example4.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Integer
+    let age: i32 = 25;
+
+    // Floating-point number
+    let height: f64 = 5.9;
+
+    // Boolean
+    let is_learning: bool = true;
+
+    // Character
+    let grade: char = 'A';
+
+    println!("Age: {age}");
+    println!("Height: {height}");
+    println!("Learning Rust: {is_learning}");
+    println!("Grade: {grade}");
+}
