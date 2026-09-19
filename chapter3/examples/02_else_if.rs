02a6a278295f68ff654b8ba1775cf952f842eb54
Arshadahamed45 <arshadahmadr@Technoidentity.com>
2026-09-19 12:49:30 +0530

    Chapter 3 else_if example

---
+++ chapter3/examples/02_else_if.rs
@@ -0,0 +1,12 @@
+fn main() {
+   let number=6;
+   if number % 4 ==0 {
+        println!("Divisible by 4");
+   }  else if number % 3 ==0 {
+        println!("Divisible by 3");
+   }  else if number % 2 ==0 {
+        println!("Divisible by 2");
+   }  else {
+        println!("None of these");
+   }
+}
