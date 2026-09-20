6d94d0bd962e45af3a48f3d70d94b215119aeffe
Arshadahamed45 <arshadahmadr@Technoidentity.com>
2026-09-20 23:16:37 +0530

    Largest Number Example

---
+++ Practice/examples/Largest.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let a=10;
+    let b=20;
+    let c=15;
+
+    if a>b && a>c{
+        println!("{a} is Largest");
+    }else if b>a && b>c{
+        println!("{b} is Largest");
+    }else{
+        println!("{c} is Largest");
+    }
+}
